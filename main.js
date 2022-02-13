74e3119f6dd1abf923b232888a0ee23e834593b5
IversonGDias <diasiverson7@gmail.com>
2022-02-13 14:34:02 -0300

    interdisciplinar

---
+++ main.js
@@ -0,0 +1,52 @@
+// abre e fecha menu qaundo clica no icone
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// quando clicar em um item do menu, econder o menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// mudar o header da pagina quando der o scroll
+function changeHeaderwhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+
+//scrollreveal: mostrar elementos qundo der scroll na página
+const scrollReveal = ScrollReveal({
+  oringin: 'top',
+  distance: '30px',
+  duraation: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
